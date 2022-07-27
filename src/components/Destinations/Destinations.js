adc96c574a18f9549a6710bd59c1f0fd1bb5db6a
Tahmina Rahman <tahminarahmansurovi@gmail.com>
2022-07-28 03:24:18 +0600

    Add to cart button and initial cart setup

--- src/components/Destinations/Destinations.js
+++ src/components/Destinations/Destinations.js
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import Place from '../Place/Place';
-import './Destinations.css'
+    import React, { useEffect, useState } from 'react';
+    import Place from '../Place/Place';
+    import Cart from '../Cart/Cart';
 
-const Destinations = () => {
+    import './Destinations.css'
 
-    const [destinations,setDestinations] = useState([]);
+    const Destinations = () => {
 
-    useEffect(() =>{
-        fetch(`data.json`)
-        .then(res => res.json())
-        .then(data => setDestinations(data))
-    },[]);
+        const [destinations,setDestinations] = useState([]);
+        const [cart, setCart] = useState([]);
 
-    return (
-        <div className='destinations'>
-       <div className="place-section">
-        {
-         
-         destinations.map(destination =><Place
-         key={destination.id}
-            destination={destination}
-            ></Place>)
+        useEffect(() =>{
+            fetch(`data.json`)
+            .then(res => res.json())
+            .then(data => setDestinations(data))
+        },[]);
+
+        const handleAddToCart = destination => {
+
+            const newCart = [...cart,destination];
+            setCart(newCart);
         }
 
-       </div>
-       <div className="cart-section">
-        
-        </div>    
-        </div>
-    );
-};
+        const RemoveCart = () => {
+        setCart([]);
+        }
 
-export default Destinations;
+        return (
+            <div className='destinations'>
+        <div className="place-section">
+            {
+            
+            destinations.map(destination =><Place
+            key={destination.id}
+                destination={destination}
+                handleAddToCart={handleAddToCart}
+                ></Place>)
+            }
+
+        </div>
+        <div className="cart-section">
+            
+            <Cart></Cart>
+
+            </div>    
+
+            </div>
+        );
+    };
+
+    export default Destinations;
--- src/components/Place/Place.js
+++ src/components/Place/Place.js
@@ -1,24 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Place.css'
 
-const Place = ({destination}) => {
+const Place = ({destination,handleAddToCart}) => {
     console.log(destination);
 
     const {name,image,price} = destination;
     return (
-        <div className='product'>
+        <div className='place'>
                 <img src= {image} alt="" />
 
-                <div className='product-info'>
-                <h3 className='product-name'>{name}</h3>
+                <div className='place-info'>
+                <h3 className='place-name'>{name}</h3>
                 <h4>Travel Cost: ${price}</h4>
 
                 </div>
                 
-            {/* <button onClick= {() => handleAddToCart(product)} className='btn-cart'>
+            <button onClick= {() => handleAddToCart(destination)} className='btn-cart'>
                 <p className='btn-text'>Add to Cart</p>
                 <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
-            </button> */}
+            </button>
             
             
             </div>
